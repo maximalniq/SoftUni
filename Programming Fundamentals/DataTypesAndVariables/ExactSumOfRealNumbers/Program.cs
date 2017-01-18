2f0237300cb1ad906f22a5e6529c5c4b7d6591c8
Martin Marinov <maximalniq@abv.bg>
2017-01-18 18:24:24 +0200

    DataTypes uploaded

---
+++ Programming Fundamentals/DataTypesAndVariables/ExactSumOfRealNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExactSumOfRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+            decimal number = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                number = decimal.Parse(Console.ReadLine());
+                sum += number;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Programming Fundamentals/DataTypesAndVariables/Greeting/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hello, {0} {1}. You are {2} years old.", firstName, lastName, age);
+        }
+    }
+}
