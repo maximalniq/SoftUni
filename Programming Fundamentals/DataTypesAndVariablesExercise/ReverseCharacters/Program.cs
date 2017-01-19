5fe1ef5c518b8d5b1ff409cbf3d22ec2783da0e0
Martin Marinov <maximalniq@abv.bg>
2017-01-19 02:18:01 +0200

    TriedTo10th

---
+++ Programming Fundamentals/DataTypesAndVariablesExercise/ReverseCharacters/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            string c = Console.ReadLine();
+
+            Console.Write(c);
+            Console.Write(b);
+            Console.Write(a);
+        }
+    }
+}
