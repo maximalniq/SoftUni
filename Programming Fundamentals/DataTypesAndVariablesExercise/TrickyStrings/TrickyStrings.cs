fd6b1f8733c3eaa09501faa4a6cd33222cebaa4a
Martin Marinov <maximalniq@abv.bg>
2017-01-31 18:15:59 +0200

    Completed DataTypesExercises

---
+++ Programming Fundamentals/DataTypesAndVariablesExercise/TrickyStrings/TrickyStrings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrickyStrings
+{
+    class TrickyStrings
+    {
+        static void Main(string[] args)
+        {
+            string delimiter = Console.ReadLine();
+            int N = int.Parse(Console.ReadLine());
+            string text = string.Empty;
+            string lastPart = string.Empty;
+            for (int i = 0; i < N; i++)
+            {
+                string someText = Console.ReadLine();
+                if (i < N - 1)
+                {
+                    text += someText;
+                    text += delimiter;
+                }
+                else if (i == N - 1)
+                {
+                    lastPart += someText;
+                }
+
+
+            }
+            Console.Write(text);
+            Console.WriteLine(lastPart);
+        }
+    }
+}
