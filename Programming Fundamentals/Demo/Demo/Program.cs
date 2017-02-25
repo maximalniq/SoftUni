538d29692cfe27653765718880931d2731d2af5e
Martin Marinov <maximalniq@abv.bg>
2017-02-25 13:53:00 +0200

    ListsMoreExercisesCompleted

---
+++ Programming Fundamentals/Demo/Demo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var List = new List<string>();
+
+            for (int i = 0; ; i++)
+            {
+                List[i] = Console.ReadLine();
+                if (List[i] == "end")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
