5b4ab0e00e09e0e3c59db2b609673e0c9ac5fba7
Martin Marinov <maximalniq@abv.bg>
2017-03-18 08:07:33 +0200

    DictionariesExercisesCompleted

---
+++ Programming Fundamentals/DictionariesExercises/MixedPhones/MixedPhones.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixedPhones
+{
+    class MixedPhones
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split(new char[] { ' ', ':' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            var sortedPhonebook = new SortedDictionary<string, long>();
+
+            while (input[0] != "Over")
+            {
+                long number;
+                bool parsed = long.TryParse(input[1], out number);
+
+                if (parsed)
+                {
+                    sortedPhonebook.Add(input[0], number);
+                }
+                else
+                {
+                    int numberReversed = Int32.Parse(input[0]);
+                    sortedPhonebook.Add(input[1], numberReversed);
+                }
+
+
+                 input = Console.ReadLine()
+                .Split(new char[] { ' ', ':' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            }
+
+            foreach (var kvp in sortedPhonebook)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
